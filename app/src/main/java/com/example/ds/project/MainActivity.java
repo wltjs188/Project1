a6a867c10b7dd6863427369f8068dcb85b92ede9
wltjs188 <wltjs188@gamil.com>
2018-05-25 12:49:58 +0900

    메인화면 변경메인화면 인텐트 추가

--- app/src/main/java/com/example/ds/project/MainActivity.java
+++ app/src/main/java/com/example/ds/project/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.ds.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(getApplicationContext(),RestListActivity.class);
+    }
+
+    public void onFood1Clicked(View view) {
+        intent.putExtra("genre","식사류");
+        startActivity(intent);
+    }
+
+    public void onFood2Clicked(View view) {
+        intent.putExtra("genre","카페/음료");
+        startActivity(intent);
+    }
+
+    public void onFood3Clicked(View view) {
+        intent.putExtra("genre","디저트");
+        startActivity(intent);
     }
 }

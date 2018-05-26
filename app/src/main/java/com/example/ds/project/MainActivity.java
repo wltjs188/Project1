0f4044398e8c530c48a35d41f77d552a72355795
wltjs188 <wltjs188@gamil.com>
2018-05-26 14:38:16 +0900

    로그인버튼 추가(사진크기변경해야됨)

--- app/src/main/java/com/example/ds/project/MainActivity.java
+++ app/src/main/java/com/example/ds/project/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.ds.project;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent1;
     Intent intent2;
+    Intent intent3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intent1 = new Intent(getApplicationContext(),RestListActivity.class);
         intent2 = new Intent(getApplicationContext(),JoinActivity.class);
+        intent3 = new Intent(getApplicationContext(),LoginActivity.class);
     }
 
     public void onFood1Clicked(View view) {
@@ -34,5 +39,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void onJoinClicked(View view) {
         startActivity(intent2);
+    }
+    //타이틀바에 로그인버튼 넣기
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    //로그인눌러질때
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+        if(id==R.id.login){
+            startActivity(intent3);
+            return true;
+        }
+        return true;
+    }
+    //액션바 숨기기
+    private void hideActionBar(){
+        ActionBar actionBar = getSupportActionBar();
+
     }
 }

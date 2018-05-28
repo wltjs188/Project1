47eca497d72d27eed2a374e9f2e6153b9274d54c
wltjs188 <wltjs188@gamil.com>
2018-05-28 19:44:10 +0900

    시바레 데이터 가져오기 성공

--- app/src/main/java/com/example/ds/project/RandomActivity.java
+++ app/src/main/java/com/example/ds/project/RandomActivity.java
@@ -3,19 +3,44 @@ package com.example.ds.project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RandomActivity extends AppCompatActivity {
     TextView title;
     Intent intent1;
     String str;
+    FirebaseDatabase database;
+    DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
+        database=FirebaseDatabase.getInstance();
+        mDatabase=database.getReference();
         title = (TextView)findViewById(R.id.title);
         intent1 = getIntent();
         str=intent1.getStringExtra("genre");
         title.setText(str);
+        mDatabase.child("test").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Log.d("test","제발"+snapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
